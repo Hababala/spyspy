dfabb020966573f4941a850de22bc0df31d54736
Nik <nikita_goldin@hotmail.com>
2024-11-23 16:19:03 +0000

    Update dependencies and simplify imports

--- spyspy.py
+++ spyspy.py
@@ -1,16 +1,7 @@
 import streamlit as st
 import plotly.graph_objects as go
 import yfinance as yf  
-import numpy as np
-from scipy import stats
 import pandas as pd
-from openbb import obb
-
-#git add .
-#git commit -m "Your new commit message"
-#git push
-
-#lets publish something to a new streamlit called spyspy
 
 st.title("spyspy")
 st.write("this is a spy game")
